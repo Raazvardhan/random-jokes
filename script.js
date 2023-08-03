9d27985b530b7426b80ff68569b1d0f114e3d340
Raj vardhan <139915269+Raazvardhan@users.noreply.github.com>
2023-08-03 15:04:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,19 +3,17 @@ const jokebutton = document.getElementById('joke-Btn');
 
 const generaterjoke = () =>{
   const xhr = new XMLHttpRequest();
-  //for english joke
- // xhr.open('GET','https://api.chucknorris.io/jokes/random');
  
- xhr.open('GET',' https://hindi-jokes-api.onrender.com/jokes?api_key=80522be835a58ef0554fd043afe6');
+ xhr.open('GET','https://api.chucknorris.io/jokes/random');
+
    
   xhr.onreadystatechange = function (){
     if(this.readyState === 4){
       if(this.status === 200){
         //console.log(JSON.parse(this.responseText).value);
           
-        jokeElement.innerHTML =JSON.parse(this.responseText).jokeContent; 
-        //for english jokes
-       //jokeElement.innerHTML =JSON.parse(this.responseText).value; 
+     
+       jokeElement.innerHTML =JSON.parse(this.responseText).value; 
 
 
       }else{
